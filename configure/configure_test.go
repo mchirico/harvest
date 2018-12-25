e202ec95abdcb97d9ad348f336944336c8621917
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-12-25 11:34:07 -0500

    files

--- configure/configure_test.go
+++ configure/configure_test.go
@@ -2,8 +2,10 @@ package configure
 
 import (
 	"encoding/json"
+	"github.com/levigross/grequests"
 	"log"
 	"os/user"
+	"strings"
 	"testing"
 )
 
@@ -50,4 +52,81 @@ func TestGetSecret(t *testing.T) {
 		t.FailNow()
 	}
 
+}
+
+func TestCodeToPassStruct(t *testing.T) {
+	c := CodeToPassStruct{}
+	c.Secret = "secret"
+	c.Id = "id"
+	c.Code = "code"
+	c.GrantType = "grant type"
+
+	result, err := c.Marshel()
+	if err != nil {
+		t.FailNow()
+	}
+	expectedResult := `{"code":"code","client_id":"id","client_secret":"secret","grant_type":"grant type"}`
+	if string(result) != expectedResult {
+		t.FailNow()
+	}
+
+}
+
+func TestInvalidGetAccessToken(t *testing.T) {
+	c := CodeToPassStruct{}
+	c.Secret = "t-xqP"
+	c.Id = "sqXYl52"
+	c.Code = "1453"
+	c.GrantType = "authorization_code"
+
+	result, err := c.Marshel()
+	if err != nil {
+		t.FailNow()
+	}
+
+	ro := grequests.RequestOptions{}
+	ro.JSON = result
+	url := "https://id.getharvest.com/api/v2/oauth2/token"
+	r, err := grequests.Post(url, &ro)
+
+	if strings.Contains(r.String(), "error") {
+		type E struct {
+			Error     string `json:"error"`
+			ErrorDesc string `json:"error_description"`
+		}
+		e := E{}
+		r.JSON(&e)
+		if e.Error != "" {
+			log.Printf("We have a problem: %v", e.ErrorDesc)
+		}
+	}
+
+}
+
+/*
+
+{"access_token":"14IlpeGg","refresh_token":"14jzGD_rwi",
+"token_type":"bearer","expires_in":1209599}
+
+*/
+
+func TestWriteResponseDataToFile(t *testing.T) {
+
+	access := "1453046"
+	ro := grequests.RequestOptions{}
+	headers := map[string]string{}
+	headers["Content-Type"] = "application/json"
+	ro.Headers = headers
+
+	s := ResultData{Access: access,
+		Refresh: "14jzGD_rwi", Type: "bearer",
+		Expires: 1209599}
+	ro.JSON = s
+	url := "http://httpbin.org/post"
+
+	result, _ := grequests.Post(url, &ro)
+	url = "http://httpbin.org/get"
+
+	//url = "https://id.getharvest.com/api/v2/accounts"
+	WriteResponseDataToFile(result, url)
 }
--- configure/setup.go
+++ configure/setup.go
@@ -1,5 +1,14 @@
 package configure
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/levigross/grequests"
+	"log"
+	"os/user"
+)
+
 type SecretStruct struct {
 	Id      string `json:"clientID"`
 	Secret  string `json:"clientSecret"`
@@ -7,4 +16,97 @@ type SecretStruct struct {
 	Code    string `json:"code"`
 	Seconds string `json:"seconds"`
 	Expire  string `json:"expire"`
+}
+
+type CodeToPassStruct struct {
+	Code      string `json:"code"`
+	Id        string `json:"client_id"`
+	Secret    string `json:"client_secret"`
+	GrantType string `json:"grant_type"`
+}
+
+type ResultData struct {
+	Access  string `json:"access_token"`
+	Refresh string `json:"refresh_token"`
+	Type    string `json:"token_type"`
+	Expires int    `json:"expires_in"`
+}
+
+func (c *CodeToPassStruct) Marshel() ([]byte, error) {
+	return json.Marshal(*c)
+}
+
+func WriteResponseDataToFile(response *grequests.Response, url string) (int, error) {
+
+	log.Printf("WriteResponseDataToFile")
+
+	var f interface{}
+	err := response.JSON(&f)
+	if err != nil {
+		return -1, errors.New("Error response.JSON(&f) ")
+	}
+
+	m := f.(map[string]interface{})
+	if m == nil {
+		return -1, errors.New("Error m := f.(map[string]interface{}) ")
+	}
+
+	r := m["json"].(map[string]interface{})
+
+	if val, ok := r["access_token"]; ok {
+		ro := grequests.RequestOptions{}
+		headers := map[string]string{}
+		headers["Content-Type"] = "application/json"
+		headers["Authorization"] = fmt.Sprintf("Bearer %v", val)
+		headers["User-Agent"] = "AiPiggybot (mchirico@gmail.com)"
+		ro.Headers = headers
+		result, err := grequests.Get(url, &ro)
+
+		if err != nil {
+			log.Printf("ERROR:  %v\n", err)
+			return -1, err
+		}
+
+		type Account struct {
+			Id      int    `json:"id"`
+			Name    string `json:"name"`
+			Product string `json:"product"`
+			Google  bool   `json:"google_sign_in_required"`
+		}
+
+		type AccountStruct struct {
+			User     interface{} `json:"user"`
+			Accounts []Account   `json:"accounts"`
+		}
+
+		v := AccountStruct{}
+		err = result.JSON(&v)
+
+		if err != nil {
+			return -1, err
+		}
+
+		if len(v.Accounts) == 0 {
+			return -1, errors.New("Didn't get ID")
+		}
+
+		log.Println(v.Accounts[0].Id)
+		usr, _ := user.Current()
+		file := usr.HomeDir + fmt.Sprintf("/.harvestData_%v", v.Accounts[0].Id)
+
+		type Store struct {
+			Access string `json:"access"`
+			Id     int    `json:"id"`
+		}
+
+		store := Store{}
+		store.Access = val.(string)
+		store.Id = v.Accounts[0].Id
+		data, err := json.Marshal(store)
+		n, err := writeFile(string(data), file)
+		return n, err
+	}
+	log.Printf("We did not write")
+	return -1, errors.New("Can't work with result")
+
 }
